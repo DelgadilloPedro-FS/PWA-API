b3b1ffdb599645e0732f264ba6fa1c8ff72bd14c
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2024-03-10 15:30:58 -0500

    fix merge conflict

---
+++ controllers/user.js
@@ -0,0 +1,11 @@
+
+
+const signup = (req, res) => {
+  res.status(200).json({
+    message: "User has sucessfully signed up!",
+    user:{...req.body,
+    password: req.body.password.split("").map((char) => "●").join("")},
+  });
+};
+
+module.exports = {signup};
---
+++ routers/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const userCtlr = require('../controllers/user');
+const utils = require('../middlewares/utils');
+
+router.post('/signup', utils.vaildateEmail, utils.vaildatePhone, utils.vaildateBirthday, utils.vaildatePassword, userCtlr.signup);
+
+module.exports = router;
