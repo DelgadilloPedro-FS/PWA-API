c06bb822b700f5b950f7fee579a1b0cf40cdd654
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2024-03-31 16:30:02 -0500

    add swagger ui to api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const userRouter = require('./routers/user')
+const userRouter = require("./routers/user.js");
+const swaggerRouter = require("./routers/swagger.js");
 
 const app = express()
 const PORT = 3000;
@@ -9,8 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/', userRouter)
-
-
+app.use('/',swaggerRouter)
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
